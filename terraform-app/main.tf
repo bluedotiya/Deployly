1998456c86504bde3987587014f5804883a1825b
Oran <ooorannn@gmail.com>
2024-12-22 15:55:17 +0000

    * Added sas token policy

--- terraform-app/main.tf
+++ terraform-app/main.tf
@@ -82,15 +82,27 @@ resource "random_id" "random_id" {
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
-  name                          = "diag${random_id.random_id.hex}"
-  location                      = azurerm_resource_group.rg.location
-  resource_group_name           = azurerm_resource_group.rg.name
-  account_tier                  = "Standard"
-  account_replication_type      = "GRS" # Changed replication to Geo-redundant storage to ensure high availablity
-  local_user_enabled            = false # Disabled local users in favor of Manganged identity if usecase arises
-  public_network_access_enabled = false # CKV_AZURE_59 - Disable public access to storage account
+  name                            = "diag${random_id.random_id.hex}"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS" # Changed replication to Geo-redundant storage to ensure high availablity
+  local_user_enabled              = false # Disabled local users in favor of Manganged identity if usecase arises
+  public_network_access_enabled   = false # CKV_AZURE_59 - Disable public access to storage account
   allow_nested_items_to_be_public = false # CKV_AZURE_190 - Block blob public access
-  min_tls_version               = "TLS1_2" # Explicitly mark TLS Version to 1.2
+  shared_access_key_enabled       = false # CKV2_AZURE_40 - Check
+  min_tls_version                 = "TLS1_2" # Explicitly mark TLS Version to 1.2
+
+  sas_policy { # SAS Tokens experation date, after 90 days it will automatically be revoked
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+
+  blob_properties { # Keep deleted blobs for 7 days, protects againest accidental deletion
+    delete_retention_policy {
+      days = 7
+    }
+  }
 }
 
 # Creating storage account queue retension and logging
@@ -118,11 +130,12 @@ resource "azurerm_storage_account_queue_properties" "logging_properties" {
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_D2_v4"
+  name                       = "myVM"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  network_interface_ids      = [azurerm_network_interface.my_terraform_nic.id]
+  size                       = "Standard_D2_v4"
+  allow_extension_operations = false # CKV_AZURE_50 - Disabled extensions that can potetically introduce configuration changes post deployment
 
   os_disk {
     name                 = "myOsDisk"
