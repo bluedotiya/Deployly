d1f59c1a0117662dc687bd540a365b5339fdd8b7
Oran <ooorannn@gmail.com>
2024-12-22 17:14:10 +0000

    * Disabled public access

--- terraform-app/main.tf
+++ terraform-app/main.tf
@@ -12,16 +12,17 @@ data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_key_vault" "production_key_vault" {
-  name                        = "key-vault"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  purge_protection_enabled    = true
-  soft_delete_retention_days  = 7
+  name                          = "key-vault"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  purge_protection_enabled      = true
+  soft_delete_retention_days    = 7
+  public_network_access_enabled = false # Disable public access for the key-vault
   network_acls {
-    default_action            = "Deny"
-    bypass                    = "AzureServices"
+    default_action              = "Deny"
+    bypass                      = "AzureServices"
   }
 }
 
